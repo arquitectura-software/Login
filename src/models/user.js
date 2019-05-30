9c375cf3f635296f732bd8cb745f89769fcacb1f
Alejandro Alejo <alsalejopa@unal.edu.co>
2019-05-30 08:34:10 -0500

    Creado docker-compose, listo para desplegar

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
+
 connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'Ident'
+    host            : process.env.DATABASE_HOST,
+    port            : process.env.MYSQL_PORT,
+    user            : process.env.MYSQL_USER,
+    password        : process.env.MYSQL_PASSWORD,
+    database        : process.env.MYSQL_DATABASE
 });
 
 let UserModel = {};
