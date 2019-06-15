09e00e0b78c060a9805bb97a6cc0e5def3a71574
Jann <janncarlomoreno@gmail.com>
2019-06-15 17:23:39 -0500

    seeding MySQL database

--- db/database.sql
+++ db/database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS Ident;
 use Ident;
 
 CREATE TABLE IF NOT EXISTS users (
-    
+
     id INT(10) unsigned NOT NULL AUTO_INCREMENT,
     names VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
     surnames VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS users (
 DESCRIBE users;
 
 CREATE TABLE IF NOT EXISTS passengers (
-    
+
     id INT(10) unsigned NOT NULL AUTO_INCREMENT,
     id_user INT(10) unsigned NOT NULL,
-    birthdate DATETIME NOT NULL,
+    birthdate DATE NOT NULL,
     email VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
     phone INT(16) unsigned NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS passengers (
 DESCRIBE passengers;
 
 CREATE TABLE IF NOT EXISTS crew (
-    
+
     id INT(10) unsigned NOT NULL AUTO_INCREMENT,
     id_user INT(10) unsigned NOT NULL,
     dependence VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -53,3 +53,33 @@ DESCRIBE crew;
 
 ALTER USER 'mysql'@'%' IDENTIFIED WITH mysql_native_password BY '1234';
 FLUSH PRIVILEGES;
+
+INSERT INTO users (names, surnames, username, passw)
+VALUES
+    ("David Arturo", "Mendez Velandia", "darturovela" ,"123456"),
+    ("Laura Sofia", "Mendez Velandia", "lsofiavela" ,"123456"),
+    ("Samuel Camilo", "Torres Vesga", "sacamiloto" ,"123456"),
+    ("Saray", "Fonseca", "saraseca" ,"123456"),
+    ("Sara Sofia", "Novoa Tellez", "saravoa" ,"123456"),
+    ("Mickey", "Dysney Marvel", "mickney" ,"123456"),
+    ("Daniela Maria", "Ruiz Zamora", "damaruiz" ,"123456"),
+    ("Luisa Fernanda", "Rodriguez", "luiferro" ,"123456"),
+    ("Andres Felipe", "Ramirez Alfonso", "anframirezal" ,"123456"),
+    ("Ximena Penelope", "Fonseca Asencio", "ximepenefo" ,"123456");
+
+    INSERT INTO passengers (id_user, birthdate, email, phone)
+VALUES
+    (1, "1990-12-15 12:00:00", "darturovela@gmail.com" ,3158302787),
+    (2, "1987-03-01 12:00:00", "lsofiavela@gmail.com" ,3143752255),
+    (3, "1995-02-21 12:00:00","sacamiloto@gmail.com" ,3102579249),
+    (4, "1977-08-11 12:00:00","saraseca@gmail.com" ,3138611109),
+    (5, "2014-03-01 12:00:00","saravoa@gmail.com" ,3013778966);
+
+
+INSERT INTO crew (id_user, dependence)
+VALUES
+    (6, "Restaurant"),
+    (7, "Store"),
+    (8, "Laundry"),
+    (9, "Security"),
+    (10, "Logistics");
